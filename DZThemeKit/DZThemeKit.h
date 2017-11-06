7a8b27c5dec1b61780208868bdc5def8bd74d6c6
Nikhil Nigade <nikhil@dezinezync.com>
2017-11-06 15:27:30 +0530

    Initial Commit

---
+++ DZThemeKit/DZThemeKit.h
@@ -0,0 +1,19 @@
+//
+//  DZThemeKit.h
+//  DZThemeKit
+//
+//  Created by Nikhil Nigade on 06/11/17.
+//  Copyright © 2017 Dezine Zync Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DZThemeKit.
+FOUNDATION_EXPORT double DZThemeKitVersionNumber;
+
+//! Project version string for DZThemeKit.
+FOUNDATION_EXPORT const unsigned char DZThemeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DZThemeKit/PublicHeader.h>
+
+
